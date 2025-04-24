a3fb98a2cecba12ef3d9066efb2fab3634b46f42
Hiroshi Nishito <admin@itc.tokyo>
2025-04-24 23:21:59 +0900

    Rename project to "itc-fun-tools", add deploy script for GitHub Pages, and update Vite configuration for base path. Add GitHub Actions workflow for automated deployment.

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Fun Tools
+# itc-fun-tools
 
 ## 概要
 
--- vite.config.js
+++ vite.config.js
@@ -5,9 +5,10 @@ import reactRefresh from '@vitejs/plugin-react-refresh'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server:{
-    port:8080,
+  server: {
+    port: 8080,
   },
-  publicDir:"public",
-  assetsInclude: ["robots.txt"]
+  publicDir: "public",
+  assetsInclude: ["robots.txt"],
+  base: "/itc-fun-tools/" // GitHubリポジトリ名に基づくbase設定
 })
